d8d6331d363ec338ea34ea10aa8ae2c4e1183159
Kasper Dahl Andersen <kasperdahldk@gmail.com>
2023-03-22 16:10:25 +0100

    Vue.js seems to be setup

--- app/src/main/resources/static/main.js
+++ app/src/main/resources/static/main.js
@@ -1,4 +1,4 @@
-document.getElementById("showAllRecipes").addEventListener("click", function() {
+/* document.getElementById("showAllRecipes").addEventListener("click", function() {
     fetch("/api/recipes")
         .then(function(response) {
             if (!response.ok) {
@@ -23,3 +23,43 @@ document.getElementById("showAllRecipes").addEventListener("click", function() {
             console.error("Error fetching recipes:", error);
         });
 });
+ */
+
+// Create a Vue instance
+const app = Vue.createApp({
+    // Data object to hold the state of the application
+    data() {
+        return {
+            // Array to store the fetched recipes
+            recipes: []
+        };
+    },
+    // Methods object to define functions for the application
+    methods: {
+        // Function to fetch and display all recipes
+        showAllRecipes() {
+            // Make a fetch request to the API endpoint
+            fetch('/api/recipes')
+                .then((response) => {
+                    // Check if the response is okay, otherwise throw an error
+                    if (!response.ok) {
+                        throw new Error('HTTP error ' + response.status);
+                    }
+                    // Parse the response JSON data
+                    return response.json();
+                })
+                .then((data) => {
+                    // Store the fetched recipes in the recipes array
+                    this.recipes = data;
+                })
+                .catch((error) => {
+                    // Log any errors that occur during the fetch request
+                    console.error('Error fetching recipes:', error);
+                });
+        }
+    }
+});
+
+// Mount the Vue instance to the HTML element with the ID 'app'
+app.mount('#app');
+
