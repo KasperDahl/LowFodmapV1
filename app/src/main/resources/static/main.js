446f7f10e73ef04660ffcfac3feb78dc6ac6f22c
Kasper Dahl Andersen <kasperdahldk@gmail.com>
2023-05-08 21:04:21 +0200

    Functionality of NavBar working, but style is not there

--- app/src/main/resources/static/main.js
+++ app/src/main/resources/static/main.js
@@ -17,49 +17,12 @@ async function loadSFC(url) {
 }
 
 (async () => {
+  const NavBar = await loadSFC('./components/NavBar.vue'); 
   const router = initRouter(loadSFC);
 
   const app = Vue.createApp({});
+  app.component('nav-bar', NavBar); // Register the NavBar component globally
   app.use(router);
   app.mount('#app');
 })();
 
-
-
-/* async function loadSFC(url) {
-    const options = {
-      moduleCache: {
-        vue: Vue
-      },
-      getFile(url) {
-        return fetch(url).then((res) => res.text());
-      },
-      addStyle(styleStr) {
-        const style = document.createElement('style');
-        style.textContent = styleStr;
-        document.head.appendChild(style);
-      }
-    };
-  
-    return await window['vue3-sfc-loader'].loadModule(url, options);
-  }
-  
-  (async () => {
-    const Home = await loadSFC('./pages/Home.vue');
-    const AllRecipes = await loadSFC('./pages/AllRecipes.vue');
-  
-    const routes = [
-      { path: '/', component: Home },
-      { path: '/allrecipes', component: AllRecipes }
-    ];
-  
-    const router = VueRouter.createRouter({
-      history: VueRouter.createWebHashHistory(),
-      routes,
-    });
-  
-    const app = Vue.createApp({});
-    app.use(router);
-    app.mount('#app');
-  })(); */
- 
--- app/src/main/resources/static/pages/Home.vue
+++ app/src/main/resources/static/pages/Home.vue
@@ -1,10 +1,7 @@
 <template>
     <div>
       <h1>Welcome to Low FODMAP Recipes!</h1>
-      <button class="btn btn-primary" @click="$router.push('/allrecipes')">Show all recipes</button>
-      <button class="btn btn-primary" @click="$router.push('/mealplan')">Meal Plan</button>
-      <button class="btn btn-primary" @click="$router.push('/offers')">Offers</button>
-      <button class="btn btn-primary" @click="$router.push('/shoppinglist')">Shopping List</button>
+
     </div>
   </template>
   
