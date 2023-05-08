f60160c18afe3e54dcdddc7545ea411d97296f29
Kasper Dahl Andersen <kasperdahldk@gmail.com>
2023-05-08 19:40:32 +0200

    clean-up, still working

--- app/bin/main/static/main.js
+++ app/bin/main/static/main.js
@@ -1,4 +1,3 @@
-
 async function loadSFC(url) {
     const options = {
       moduleCache: {
@@ -18,8 +17,8 @@ async function loadSFC(url) {
   }
   
   (async () => {
-    const Home = await loadSFC('./Home.vue');
-    const AllRecipes = await loadSFC('./AllRecipes.vue');
+    const Home = await loadSFC('./pages/Home.vue');
+    const AllRecipes = await loadSFC('./pages/AllRecipes.vue');
   
     const routes = [
       { path: '/', component: Home },
@@ -35,143 +34,4 @@ async function loadSFC(url) {
     app.use(router);
     app.mount('#app');
   })();
-  
-
-
-
-/* // Import components from their respective files
-const Home = httpVueLoader('./Home.vue');
-const AllRecipes = httpVueLoader('./AllRecipes.vue');
-
-const routes = [
-  { path: '/', component: Home },
-  { path: '/allrecipes', component: AllRecipes },
-];
-
-const router = VueRouter.createRouter({
-  history: VueRouter.createWebHashHistory(),
-  routes,
-});
-
-const app = Vue.createApp({});
-app.use(router);
-app.mount('#app');
- */
-
-
-
-
-/* const AllRecipes = {
-    template: `
-      <div>
-        <h2>All Recipes</h2>
-        <ul>
-          <li v-for="recipe in recipes" :key="recipe.id">{{ recipe.name }}</li>
-        </ul>
-      </div>
-    `,
-    data() {
-      return {
-        recipes: [],
-      };
-    },
-    created() {
-      fetch("/api/recipes")
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("HTTP error " + response.status);
-          }
-          return response.json();
-        })
-        .then((data) => {
-          this.recipes = data;
-        })
-        .catch((error) => {
-          console.error("Error fetching recipes:", error);
-        });
-    },
-  };
-  
-  const router = VueRouter.createRouter({
-    history: VueRouter.createWebHistory(),
-    routes: [
-      {
-        path: "/allrecipes",
-        component: AllRecipes,
-      },
-    ],
-  });
-  
-  const app = Vue.createApp({
-    template: `
-      <div>
-        <h1>Low FODMAP Recipes</h1>
-        <button class="btn btn-primary" @click="$router.push('/allrecipes')">Show all recipes</button>
-        <router-view></router-view>
-      </div>
-    `,
-  });
-  
-  app.use(router);
-  app.mount("#app"); */
-  
-
-
-
-
-
-/* import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-
-console.log('Creating app');
-
-const app = createApp(App);
-
-console.log('Initializing Vue.js app');
-app.use(router);
-app.mount('#app');
-console.log('Vue.js app initialized'); */
-
-
-
-
-/* // Create a Vue instance
-const app = Vue.createApp({
-    // Data object to hold the state of the application
-    data() {
-        return {
-            // Array to store the fetched recipes
-            recipes: []
-        };
-    },
-    // Methods object to define functions for the application
-    methods: {
-        // Function to fetch and display all recipes
-        showAllRecipes() {
-            // Make a fetch request to the API endpoint
-            fetch('/api/recipes')
-                .then((response) => {
-                    // Check if the response is okay, otherwise throw an error
-                    if (!response.ok) {
-                        throw new Error('HTTP error ' + response.status);
-                    }
-                    // Parse the response JSON data
-                    return response.json();
-                })
-                .then((data) => {
-                    // Store the fetched recipes in the recipes array
-                    this.recipes = data;
-                })
-                .catch((error) => {
-                    // Log any errors that occur during the fetch request
-                    console.error('Error fetching recipes:', error);
-                });
-        }
-    }
-});
-
-// Mount the Vue instance to the HTML element with the ID 'app'
-app.mount('#app');
-
- */
+ 
