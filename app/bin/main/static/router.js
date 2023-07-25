eb78b3f733061516487a9553c45d0474df5b9170
Kasper Dahl Andersen <kasperdahldk@gmail.com>
2023-07-25 17:17:33 +0200

    added a few recipes to json-file

---
+++ app/bin/main/static/router.js
@@ -0,0 +1,25 @@
+
+function loadRoutes(loadSFC) {
+    return [
+      { path: '/', component: () => loadSFC('./pages/Home.vue') },
+      { path: '/allrecipes', component: () => loadSFC('./pages/AllRecipes.vue') },
+      { path: '/mealplan', component: () => loadSFC('./pages/MealPlan.vue') },
+      { path: '/offers', component: () => loadSFC('./pages/Offers.vue') },
+      { path: '/shoppinglist', component: () => loadSFC('./pages/ShoppingList.vue') },
+      { path: '/recipe/:name', component: () => loadSFC('./pages/RecipeDetail.vue') },
+      // Add more routes here
+    ];
+  }
+  
+  function initRouter(loadSFC) {
+    const routes = loadRoutes(loadSFC);
+  
+    const router = VueRouter.createRouter({
+      history: VueRouter.createWebHashHistory(),
+      routes,
+    });
+  
+    return router;
+  }
+  
+  
