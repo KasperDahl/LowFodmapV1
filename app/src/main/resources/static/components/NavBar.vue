80a5844f369ac0f5e668e0e78539fd0c0ce82156
Kasper Dahl Andersen <kasperdahldk@gmail.com>
2023-07-28 13:21:42 +0200

    changed styling and removed calories

--- app/src/main/resources/static/components/NavBar.vue
+++ app/src/main/resources/static/components/NavBar.vue
@@ -1,8 +1,15 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-      <router-link class="navbar-brand" to="/">
-        <img src="/assets/logo_LFD.jpg" alt="LFD" style="height: 50px" />
+      <router-link
+        class="navbar-brand"
+        to="/"
+      >
+        <img
+          src="/assets/logo_LFD.jpg"
+          alt="LFD"
+          style="height: 50px"
+        />
       </router-link>
       <button
         class="navbar-toggler"
@@ -15,25 +22,34 @@
       >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
+      <div
+        class="collapse navbar-collapse"
+        id="navbarNav"
+      >
         <ul class="navbar-nav">
           <li class="nav-item">
-            <router-link class="nav-link" to="/allrecipes"
-              >All Recipes</router-link
-            >
+            <router-link
+              class="nav-link"
+              to="/allrecipes"
+            >All Recipes</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/uploadrecipe"
-              >Upload Recipe</router-link
-            >
+            <router-link
+              class="nav-link"
+              to="/uploadrecipe"
+            >Upload Recipe</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/offers">Offers</router-link>
+            <router-link
+              class="nav-link"
+              to="/offers"
+            >Offers</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/shoppinglist"
-              >Shopping List</router-link
-            >
+            <router-link
+              class="nav-link"
+              to="/shoppinglist"
+            >Shopping List</router-link>
           </li>
         </ul>
       </div>
--- app/src/main/resources/static/pages/UploadRecipe.vue
+++ app/src/main/resources/static/pages/UploadRecipe.vue
@@ -5,19 +5,31 @@
       <div class="form-group">
         <label for="name">Name</label>
         <br />
-        <input type="text" id="name" v-model="recipe.name" required />
+        <input
+          type="text"
+          id="name"
+          v-model="recipe.name"
+          required
+        />
       </div>
 
       <!-- Implementation for Instructions -->
       <div class="instructions">
         <h2>Instructions</h2>
-        <div v-for="(instruction, index) in instructions" :key="index">
+        <div
+          v-for="(instruction, index) in instructions"
+          :key="index"
+        >
           <p
             v-if="instruction.edit"
             style="background-color: #f5f5f5; padding: 10px"
           >
             Step {{ index + 1 }}:
-            <input type="text" v-model="instruction.text" style="width: 60%" />
+            <input
+              type="text"
+              v-model="instruction.text"
+              style="width: 60%"
+            />
             <button
               @click="addEdit(index, 'instructions')"
               class="btn btn-primary"
@@ -34,7 +46,10 @@
               Remove
             </button>
           </p>
-          <p v-else style="background-color: #f5f5f5; padding: 10px">
+          <p
+            v-else
+            style="background-color: #f5f5f5; padding: 10px"
+          >
             Step {{ index + 1 }}: {{ instruction.text }}
             <button
               @click="edit(index, 'instructions')"
@@ -67,7 +82,11 @@
             style="background-color: #f5f5f5; padding: 10px"
           >
             Name:
-            <input type="text" v-model="ingredient.name" style="width: 30%" />
+            <input
+              type="text"
+              v-model="ingredient.name"
+              style="width: 30%"
+            />
             Quantity:
             <input
               type="number"
@@ -75,7 +94,11 @@
               style="width: 8%"
             />
             Unit:
-            <input type="text" v-model="ingredient.unit" style="width: 10%" />
+            <input
+              type="text"
+              v-model="ingredient.unit"
+              style="width: 10%"
+            />
             <button
               @click="addEdit(index, 'ingredients')"
               class="btn btn-primary"
@@ -92,7 +115,10 @@
               Remove
             </button>
           </p>
-          <p v-else style="background-color: #f5f5f5; padding: 10px">
+          <p
+            v-else
+            style="background-color: #f5f5f5; padding: 10px"
+          >
             Name: {{ ingredient.name }} Quantity:
             {{ ingredient.quantity }} Unit: {{ ingredient.unit }}
             <button
@@ -113,67 +139,87 @@
           </p>
         </div>
       </div>
-      <!-- Serving size div -->
-      <div class="form-group">
-        <label for="servings">Servings</label>
-        <br />
-        <select id="servings" v-model="recipe.servings" required>
-          <option v-for="number in 8" :value="number" :key="number">
-            {{ number }}
-          </option>
-        </select>
-      </div>
 
-      <!-- Meal Type div -->
-      <div class="form-group">
-        <label for="mealType">Meal Type</label>
-        <br />
-        <select id="mealType" v-model="recipe.mealType" required>
-          <option value="Hovedret">Hovedret</option>
-          <option value="Forret">Forret</option>
-          <option value="Dessert">Dessert</option>
-          <option value="Salat">Salat</option>
-        </select>
+      <div class="d-flex justify-content-between mb-3">
+        <!-- Serving size div -->
+        <div class="form-group">
+          <label for="servings">Servings</label>
+          <br />
+          <select
+            id="servings"
+            v-model="recipe.servings"
+            class="form-select"
+            required
+          >
+            <option
+              v-for="number in 8"
+              :value="number"
+              :key="number"
+            >{{ number }}</option>
+          </select>
+        </div>
+        <!-- Meal Type div -->
+        <div class="form-group">
+          <label for="mealType">Meal Type</label>
+          <br />
+          <select
+            id="mealType"
+            v-model="recipe.mealType"
+            class="form-select"
+            required
+          >
+            <option value="Hovedret">Hovedret</option>
+            <option value="Forret">Forret</option>
+            <option value="Dessert">Dessert</option>
+            <option value="Salat">Salat</option>
+          </select>
+        </div>
+        <!-- Full Meal div -->
+        <div class="form-group">
+          <label for="fullMeal">Full Meal</label>
+          <br />
+          <select
+            id="fullMeal"
+            v-model="recipe.fullMeal"
+            class="form-select"
+            required
+          >
+            <option value="true">True</option>
+            <option value="false">False</option>
+          </select>
+        </div>
+        <!-- Time To Cook div -->
+        <div class="form-group">
+          <label for="timeToCook">Time To Cook (in minutes)</label>
+          <br />
+          <input
+            type="number"
+            id="timeToCook"
+            v-model="recipe.timeToCook"
+            class="form-control"
+            required
+          />
+        </div>
       </div>
-
-      <!-- Full Meal div -->
-      <div class="form-group">
-        <label for="fullMeal">Full Meal</label>
-        <br />
-        <select id="fullMeal" v-model="recipe.fullMeal" required>
-          <option value="true">True</option>
-          <option value="false">False</option>
-        </select>
-      </div>
-
-      <!-- Calories div -->
-      <div class="form-group">
-        <label for="calories">Calories</label>
-        <br />
-        <input type="number" id="calories" v-model="recipe.calories" required />
-      </div>
-
-      <!-- Time To Cook div -->
-      <div class="form-group">
-        <label for="timeToCook">Time To Cook</label>
-        <br />
-        <input
-          type="number"
-          id="timeToCook"
-          v-model="recipe.timeToCook"
-          required
-        />
-      </div>
-
       <!-- Comments div -->
       <div class="form-group">
         <label for="comments">Comments</label>
         <br />
-        <textarea id="comments" v-model="recipe.comments" required />
+        <textarea
+          id="comments"
+          v-model="recipe.comments"
+          class="form-control"
+          style="height: 100px;"
+          required
+        ></textarea>
       </div>
 
       <div>
-        <button type="submit" class="btn btn-primary" style="float: left">
+        <button
+          type="submit"
+          class="btn btn-primary"
+          style="float: left"
+        >
           Save Recipe
         </button>
       </div>
@@ -192,7 +238,6 @@ export default {
         servings: 0,
         mealType: "",
         fullMeal: false,
-        calories: 0,
         timeToCook: "",
         comments: "",
       },
@@ -239,292 +284,3 @@ export default {
   },
 };
 </script>
-
-<!-- <template>
-  <div class="recipe-upload">
-    <h1>Upload Recipe</h1>
-    <form @submit.prevent="submitRecipe">
-      <div class="form-group">
-        <label for="name">Name</label>
-        <br />
-        <input type="text" id="name" v-model="recipe.name" required />
-      </div>
-
-   
-      <div class="instructions">
-        <h2>Instructions</h2>
-        <div v-for="(instruction, index) in instructions" :key="index">
-          <p
-            v-if="instruction.edit"
-            style="background-color: #f5f5f5; padding: 10px"
-          >
-            Step {{ index + 1 }}:
-            <input type="text" v-model="instruction.text" style="width: 70%" />
-            <button
-              @click="addEdit(index, 'instructions')"
-              class="btn btn-primary"
-              style="margin-left: 5px"
-            >
-              Add
-            </button>
-            <button
-              @click="remove(index, 'instructions')"
-              class="btn btn-primary"
-              style="margin-left: 5px"
-            >
-              Remove
-            </button>
-          </p>
-          <p v-else style="background-color: #f5f5f5; padding: 10px">
-            Step {{ index + 1 }}: {{ instruction.text }}
-            <button
-              @click="edit(index, 'instructions')"
-              class="btn btn-primary"
-            >
-              Edit
-            </button>
-            <button
-              @click="remove(index, 'instructions')"
-              class="btn btn-primary"
-              style="margin-left: 5px"
-            >
-              Remove
-            </button>
-          </p>
-        </div>
-      </div>
-
-     
-      <div class="ingredients">
-        <h2>Ingredients</h2>
-        <div
-          v-for="(ingredient, index) in ingredients"
-          :key="'ingredient' + index"
-        >
-          <p
-            v-if="ingredient.edit"
-            style="background-color: #f5f5f5; padding: 10px"
-          >
-            Name:
-            <input type="text" v-model="ingredient.name" style="width: 30%" />
-            Quantity:
-            <input
-              type="number"
-              v-model="ingredient.quantity"
-              style="width: 20%"
-            />
-            Unit:
-            <input type="text" v-model="ingredient.unit" style="width: 20%" />
-            <button
-              @click="addEdit(index, 'ingredients')"
-              class="btn btn-primary"
-              style="margin-left: 5px"
-            >
-              Add
-            </button>
-            <button
-              @click="remove(index, 'ingredients')"
-              class="btn btn-primary"
-              style="margin-left: 5px"
-            >
-              Remove
-            </button>
-          </p>
-          <p v-else style="background-color: #f5f5f5; padding: 10px">
-            Name: {{ ingredient.name }} Quantity:
-            {{ ingredient.quantity }} Unit: {{ ingredient.unit }}
-            <button @click="edit(index, 'ingredients')" class="btn btn-primary">
-              Edit
-            </button>
-            <button
-              @click="remove(index, 'ingredients')"
-              class="btn btn-primary"
-              style="margin-left: 5px"
-            >
-              Remove
-            </button>
-          </p>
-        </div>
-      </div>
-      <div>
-        <button type="submit" class="btn btn-primary" style="float: left">
-          Save Recipe
-        </button>
-      </div>
-    </form>
-  </div>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      recipe: {
-        name: "",
-        instructions: [],
-        ingredients: [],
-        servings: 0,
-        mealType: "",
-        fullMeal: false,
-        calories: 0,
-        timeToCook: "",
-        comments: "",
-      },
-      instructions: [{ text: "", edit: true }],
-      ingredients: [{ name: "", quantity: "", unit: "", edit: true }],
-    };
-  },
-  methods: {
-    add(property) {
-      if (property === "instructions") {
-        this.instructions.push({ text: "", edit: true });
-      } else if (property === "ingredients") {
-        this.ingredients.push({ name: "", quantity: "", unit: "", edit: true });
-      }
-    },
-    edit(index, property) {
-      if (property === "instructions") {
-        this.instructions[index].edit = true;
-      } else if (property === "ingredients") {
-        this.ingredients[index].edit = true;
-      }
-    },
-    addEdit(index, property) {
-      if (property === "instructions") {
-        this.instructions[index].edit = false;
-        this.add("instructions");
-      } else if (property === "ingredients") {
-        this.ingredients[index].edit = false;
-        this.add("ingredients");
-      }
-    },
-    remove(index, property) {
-      if (property === "instructions") {
-        this.instructions.splice(index, 1);
-      } else if (property === "ingredients") {
-        this.ingredients.splice(index, 1);
-      }
-    },
-    submitRecipe() {
-      // Here, you can process the final object to be sent to the backend
-      console.log(this.recipe);
-    },
-  },
-};
-</script> -->
-
-<!-- <template>
-  <div class="recipe-upload">
-    <h1>Upload Recipe</h1>
-    <form @submit.prevent="submitRecipe">
-      <div class="form-group">
-        <label for="name">Name</label>
-        <br />
-        <input type="text" id="name" v-model="recipe.name" required />
-      </div>
-
-      
-
-      <div class="instructions">
-        <h2>Instructions</h2>
-        <div v-for="(instruction, index) in instructions" :key="index">
-          <p
-            v-if="instruction.edit"
-            style="background-color: #f5f5f5; padding: 10px"
-          >
-            Step {{ index + 1 }}:
-            <input type="text" v-model="instruction.text" style="width: 70%" />
-            <button
-              @click="addEditInstruction(index)"
-              class="btn btn-primary"
-              style="float: right; margin-left: 5px"
-            >
-              Add
-            </button>
-            <button
-              @click="removeInstruction(index)"
-              class="btn btn-primary"
-              style="float: right"
-            >
-              Remove
-            </button>
-          </p>
-          <p v-else style="background-color: #f5f5f5; padding: 10px">
-            Step {{ index + 1 }}: {{ instruction.text }}
-            <button
-              @click="editInstruction(index)"
-              class="btn btn-primary"
-              style="float: right; margin-left: 5px"
-            >
-              Edit
-            </button>
-            <button
-              @click="removeInstruction(index)"
-              class="btn btn-primary"
-              style="float: right"
-            >
-              Remove
-            </button>
-          </p>
-        </div>
-        <button
-          @click.prevent="addStep"
-          class="btn btn-primary"
-          style="float: right"
-        >
-          Add Step
-        </button>
-        <br />
-        <button type="submit" class="btn btn-primary" style="float: left">
-          Save Recipe
-        </button>
-      </div>
-
-      
-    </form>
-  </div>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      recipe: {
-        name: "",
-        instructions: "",
-        ingredients: [],
-        servings: 0,
-        mealType: "",
-        fullMeal: false,
-        calories: 0,
-        timeToCook: "",
-        comments: "",
-      },
-      newInstruction: "",
-      newIngredient: { name: "", quantity: 0, unit: "" },
-      instructions: [{ text: "", edit: true }],
-      ingredients: [],
-    };
-  },
-  methods: {
-    addStep() {
-      this.instructions.push({ text: "", edit: true });
-    },
-    editInstruction(index) {
-      this.instructions[index].edit = true;
-    },
-    addEditInstruction(index) {
-      this.instructions[index].edit = false;
-      this.addStep();
-    },
-    removeInstruction(index) {
-      this.instructions.splice(index, 1);
-    },
-    // similar methods for ingredients
-    submitRecipe() {
-      // Here, you can process the final object to be sent to the backend
-      console.log(this.recipe);
-    },
-  },
-};
-</script> -->
